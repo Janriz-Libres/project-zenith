390e26761d81638b23e3ef98e7c75fb3b99f81ca
dioexsys <dioexsys@gmail.com>
2023-11-27 10:13:06 +0800

    modified layout

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:project_zenith/pages/auth_page.dart';
 import 'package:project_zenith/pages/welcome_page.dart';
+import 'package:project_zenith/widgets/copyright_mark.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
