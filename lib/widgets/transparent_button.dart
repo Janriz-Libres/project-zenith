f4d506316a209314f57456edf99d7ad859821153
dioexsys <dioexsys@gmail.com>
2023-11-27 11:16:03 +0800

    created transparent button

---
+++ lib/widgets/transparent_button.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class TransparentButton extends StatelessWidget {
+  final String text;
+  final Color flat;
+  final Color hovered;
+  final Color lineColor;
+
+  const TransparentButton({
+    super.key,
+    required this.text,
+    required this.flat,
+    required this.hovered,
+    required this.lineColor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.resolveWith<Color>(
+        (Set<MaterialState> states) {
+        if (states.contains(MaterialState.hovered)) {
+          return hovered;
+        }
+        return flat; // null throus error in flutter 2.2+.
+        }),
+      ),
+      onPressed: () { },
+      child: FittedBox(
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            fontFamily: 'DM Sans',
+            fontWeight: FontWeight.w700,
+            decoration: TextDecoration.underline,
+            decorationColor: lineColor,
+            height: 0,
+          ),
+        ),
+      ),
+    );
+    /* return Container(
+      color: Colors.transparent,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.transparent,
+        ),
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontFamily: 'DM Sans',
+            fontWeight: FontWeight.w700,
+            height: 0,
+          ),
+        ),
+      ),
+    ); 
+  */
+  }
+}
