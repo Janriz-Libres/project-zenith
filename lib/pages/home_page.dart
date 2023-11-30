047f738d04a11c44268fe5f89b9a356e02c4f1e6
dioexsys <dioexsys@gmail.com>
2023-11-30 09:15:06 +0800

    modified layout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,151 +23,111 @@ class _HomePageState extends State<HomePage> {
       body: Row(
         children: [
           Flexible(
-            child: ConstrainedBox(
+            child: Container(
               constraints: const BoxConstraints(minWidth: 330, maxWidth: 385),
-              child: Container(
-                padding: const EdgeInsets.only(top: 25),
-                decoration: const BoxDecoration(
-                  color: Color(0xFFD9D9D9),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0x3F000000),
-                      blurRadius: 4,
-                      offset: Offset(2, 0),
-                      spreadRadius: 0,
-                    )
-                  ],
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 29),
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 35,
-                            height: 35,
-                            decoration: const ShapeDecoration(
-                              color: Color(0xFF313638),
-                              shape: OvalBorder(),
-                            ),
-                          ),
-                          const Spacer(),
-                          Expanded(
-                            flex: 18,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  widget.username,
-                                  style: const TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 20,
-                                    fontFamily: 'Rubik',
-                                    fontWeight: FontWeight.w400,
-                                    height: 0,
-                                  ),
-                                ),
-                                Text(
-                                  widget.emailAddress,
-                                  style: const TextStyle(
-                                    color: Color(0xFF636769),
-                                    fontSize: 15,
-                                    fontFamily: 'Rubik',
-                                    fontWeight: FontWeight.w400,
-                                    height: 0,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: double.maxFinite,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          DrawOption(
-                            imgPath: "assets/white_logo.png",
-                            text: "Logout",
-                            func: () async {
-                              await Authenticator.logout();
-
-                              SharedPreferences prefs =
-                                  await SharedPreferences.getInstance();
-                              prefs.clear();
-
-                              if (context.mounted) {
-                                Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const AuthPage(),
-                                  ),
-                                );
-                              }
-                            },
-                          ),
-                          DrawOption(
-                            imgPath: "assets/build_icon.png",
-                            text: "Profile",
-                            func: () {},
-                          ),
-                          DrawOption(
-                            imgPath: "assets/later_icon.png",
-                            text: "Attendance",
-                            func: () {},
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 24),
-                      child: Row(children: [
-                        const Text(
-                          'WORKSPACE',
-                          style: TextStyle(
-                            color: Color(0xFF959A9C),
-                            fontSize: 16,
-                            fontFamily: 'Rubik',
-                            fontWeight: FontWeight.w700,
-                            height: 0,
+              padding: const EdgeInsets.only(top: 25),
+              decoration: const BoxDecoration(
+                color: Color(0xFFD9D9D9),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x3F000000),
+                    blurRadius: 4,
+                    offset: Offset(2, 0),
+                    spreadRadius: 0,
+                  )
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 29),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 35,
+                          height: 35,
+                          decoration: const ShapeDecoration(
+                            color: Color(0xFF313638),
+                            shape: OvalBorder(),
                           ),
                         ),
-                        const SizedBox(width: 10),
-                        IconButton(
-                          icon: const Icon(Icons.add),
-                          onPressed: () {},
-                        )
-                      ]),
-                    ),
-                    Container(
-                      constraints: const BoxConstraints(maxHeight: 150),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            DrawOption(
-                              imgPath: "assets/join_icon.png",
-                              text: "Booth Department",
-                              func: () {},
-                            ),
-                            DrawOption(
-                              imgPath: "assets/extra_icon.png",
-                              text: "Create new workspace",
-                              func: () {},
-                            ),
-                          ],
+                        const Spacer(),
+                        Expanded(
+                          flex: 18,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.username,
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontFamily: 'Rubik',
+                                  fontWeight: FontWeight.w400,
+                                  height: 0,
+                                ),
+                              ),
+                              Text(
+                                widget.emailAddress,
+                                style: const TextStyle(
+                                  color: Color(0xFF636769),
+                                  fontSize: 15,
+                                  fontFamily: 'Rubik',
+                                  fontWeight: FontWeight.w400,
+                                  height: 0,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                    const SizedBox(height: 20),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 24),
-                      child: Text(
-                        'SHARED',
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.maxFinite,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        DrawOption(
+                          imgPath: "assets/white_logo.png",
+                          text: "Logout",
+                          func: () async {
+                            await Authenticator.logout();
+            
+                            SharedPreferences prefs =
+                                await SharedPreferences.getInstance();
+                            prefs.clear();
+            
+                            if (context.mounted) {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const AuthPage(),
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                        DrawOption(
+                          imgPath: "assets/build_icon.png",
+                          text: "Profile",
+                          func: () {},
+                        ),
+                        DrawOption(
+                          imgPath: "assets/later_icon.png",
+                          text: "Attendance",
+                          func: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: Row(children: [
+                      const Text(
+                        'WORKSPACE',
                         style: TextStyle(
                           color: Color(0xFF959A9C),
                           fontSize: 16,
@@ -176,43 +136,81 @@ class _HomePageState extends State<HomePage> {
                           height: 0,
                         ),
                       ),
-                    ),
-                    Container(
-                      constraints: const BoxConstraints(maxHeight: 150),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            DrawOption(
-                              imgPath: "assets/build_icon.png",
-                              text: "Booth Department",
-                              func: () {},
-                            ),
-                            DrawOption(
-                              imgPath: "assets/build_icon.png",
-                              text: "Booth Department",
-                              func: () {},
-                            ),
-                            DrawOption(
-                              imgPath: "assets/build_icon.png",
-                              text: "Booth Department",
-                              func: () {},
-                            ),
-                            DrawOption(
-                              imgPath: "assets/build_icon.png",
-                              text: "Booth Department",
-                              func: () {},
-                            ),
-                            DrawOption(
-                              imgPath: "assets/later_icon.png",
-                              text: "Finance Department",
-                              func: () {},
-                            ),
-                          ],
-                        ),
+                      const SizedBox(width: 10),
+                      IconButton(
+                        icon: const Icon(Icons.add),
+                        onPressed: () {},
+                      )
+                    ]),
+                  ),
+                  Container(
+                    constraints: const BoxConstraints(maxHeight: 150),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          DrawOption(
+                            imgPath: "assets/join_icon.png",
+                            text: "Booth Department",
+                            func: () {},
+                          ),
+                          DrawOption(
+                            imgPath: "assets/extra_icon.png",
+                            text: "Create new workspace",
+                            func: () {},
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 24),
+                    child: Text(
+                      'SHARED',
+                      style: TextStyle(
+                        color: Color(0xFF959A9C),
+                        fontSize: 16,
+                        fontFamily: 'Rubik',
+                        fontWeight: FontWeight.w700,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    constraints: const BoxConstraints(maxHeight: 150),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          DrawOption(
+                            imgPath: "assets/build_icon.png",
+                            text: "Booth Department",
+                            func: () {},
+                          ),
+                          DrawOption(
+                            imgPath: "assets/build_icon.png",
+                            text: "Booth Department",
+                            func: () {},
+                          ),
+                          DrawOption(
+                            imgPath: "assets/build_icon.png",
+                            text: "Booth Department",
+                            func: () {},
+                          ),
+                          DrawOption(
+                            imgPath: "assets/build_icon.png",
+                            text: "Booth Department",
+                            func: () {},
+                          ),
+                          DrawOption(
+                            imgPath: "assets/later_icon.png",
+                            text: "Finance Department",
+                            func: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
