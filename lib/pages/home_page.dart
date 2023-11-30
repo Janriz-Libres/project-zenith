d41116c4ab780da53df70162519706fa4567850f
Janriz Libres <libres.janriz@gmail.com>
2023-11-30 11:44:51 +0800

    GWENCHANA

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:project_zenith/db_api.dart';
 import 'package:project_zenith/main.dart';
 import 'package:project_zenith/pages/auth_page.dart';
+import 'package:project_zenith/subpages/fresh_page.dart';
 import 'package:project_zenith/subpages/profile_page.dart';
-import 'package:project_zenith/subpages/task_page.dart';
 import 'package:project_zenith/widgets/draw_option.dart';
 import 'package:project_zenith/widgets/sidebar_list.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   final String emailAddress;
   final String username;
   const HomePage({
@@ -17,11 +17,6 @@ class HomePage extends StatefulWidget {
     required this.username,
   });
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,7 +60,7 @@ class _HomePageState extends State<HomePage> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
-                                  widget.username,
+                                  username,
                                   style: const TextStyle(
                                     color: Colors.black,
                                     fontSize: 20,
@@ -75,7 +70,7 @@ class _HomePageState extends State<HomePage> {
                                   ),
                                 ),
                                 Text(
-                                  widget.emailAddress,
+                                  emailAddress,
                                   style: const TextStyle(
                                     color: Color(0xFF636769),
                                     fontSize: 15,
@@ -91,145 +86,169 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     const SizedBox(height: 20),
-                    SidebarList(children: [
-                      SizedBox(
-                        width: double.maxFinite,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                    LayoutBuilder(builder: (context, constraints) {
+                      if (currentUser?.id == 'rISCknyu5dlIrfGrKyCp') {
+                        return SidebarList(
                           children: [
-                            DrawOption(
-                              imgPath: "assets/white_logo.png",
-                              text: "Logout",
-                              func: () async {
-                                await Authenticator.logout();
-
-                                SharedPreferences prefs =
-                                    await SharedPreferences.getInstance();
-                                prefs.clear();
-
-                                ownedWorkspaces.clear();
-                                sharedWorkspaces.clear();
-                                lists.clear();
-                                tasks.clear();
-                                currentUser = null;
-
-                                if (context.mounted) {
-                                  Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => const AuthPage(),
-                                    ),
-                                  );
-                                }
-                              },
-                            ),
-                            DrawOption(
-                              imgPath: "assets/build_icon.png",
-                              text: "Profile",
-                              func: () {},
-                            ),
-                            DrawOption(
-                              imgPath: "assets/later_icon.png",
-                              text: "Attendance",
-                              func: () {},
+                            SizedBox(
+                              child: Column(children: [
+                                DrawOption(
+                                  imgPath: 'assets/join_icon.png',
+                                  text: "Home",
+                                  func: () {},
+                                ),
+                                DrawOption(
+                                  imgPath: 'assets/join_icon.png',
+                                  text: "Attendance",
+                                  func: () {},
+                                )
+                              ]),
                             ),
                           ],
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 24),
-                        child: Row(children: [
-                          const Text(
-                            'WORKSPACE',
-                            style: TextStyle(
-                              color: Color(0xFF959A9C),
-                              fontSize: 16,
-                              fontFamily: 'Rubik',
-                              fontWeight: FontWeight.w700,
-                              height: 0,
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          IconButton(
-                            icon: const Icon(Icons.add),
-                            onPressed: () {},
-                          )
-                        ]),
-                      ),
-                      Container(
-                        constraints: const BoxConstraints(maxHeight: 150),
-                        child: ownedWorkspaces.isEmpty
-                            ? const Column(
-                                mainAxisSize: MainAxisSize.min,
-                              )
-                            : ListView.builder(
-                                shrinkWrap: true,
-                                itemCount: ownedWorkspaces.length,
-                                itemBuilder: (context, index) {
-                                  return DrawOption(
-                                    imgPath: "assets/build_icon.png",
-                                    text: ownedWorkspaces
-                                        .elementAt(index)
-                                        .title
-                                        .toString(),
-                                    func: () {},
-                                  );
+                        );
+                      }
+
+                      return SidebarList(children: [
+                        SizedBox(
+                          width: double.maxFinite,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              DrawOption(
+                                imgPath: "assets/white_logo.png",
+                                text: "Logout",
+                                func: () async {
+                                  await Authenticator.logout();
+
+                                  SharedPreferences prefs =
+                                      await SharedPreferences.getInstance();
+                                  prefs.clear();
+
+                                  ownedWorkspaces.clear();
+                                  sharedWorkspaces.clear();
+                                  lists.clear();
+                                  tasks.clear();
+                                  currentUser = null;
+
+                                  if (context.mounted) {
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => const AuthPage(),
+                                      ),
+                                    );
+                                  }
                                 },
                               ),
-                      ),
-                      const SizedBox(height: 10),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 24),
-                        child: Row(
-                          children: [
-                            const Text('SHARED',
-                                style: TextStyle(
-                                  color: Color(0xFF959A9C),
-                                  fontSize: 16,
-                                  fontFamily: 'Rubik',
-                                  fontWeight: FontWeight.w700,
-                                  height: 0,
-                                )),
+                              DrawOption(
+                                imgPath: "assets/build_icon.png",
+                                text: "Profile",
+                                func: () {},
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 24),
+                          child: Row(children: [
+                            const Text(
+                              'WORKSPACE',
+                              style: TextStyle(
+                                color: Color(0xFF959A9C),
+                                fontSize: 16,
+                                fontFamily: 'Rubik',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
                             const SizedBox(width: 10),
                             IconButton(
                               icon: const Icon(Icons.add),
                               onPressed: () {},
                             )
-                          ],
+                          ]),
                         ),
-                      ),
-                      Container(
-                        constraints: const BoxConstraints(maxHeight: 150),
-                        child: sharedWorkspaces.isEmpty
-                            ? const Column(
-                                mainAxisSize: MainAxisSize.min,
+                        Container(
+                          constraints: const BoxConstraints(maxHeight: 150),
+                          child: ownedWorkspaces.isEmpty
+                              ? const Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                )
+                              : ListView.builder(
+                                  shrinkWrap: true,
+                                  itemCount: ownedWorkspaces.length,
+                                  itemBuilder: (context, index) {
+                                    return DrawOption(
+                                      imgPath: "assets/build_icon.png",
+                                      text: ownedWorkspaces
+                                          .elementAt(index)
+                                          .title
+                                          .toString(),
+                                      func: () {},
+                                    );
+                                  },
+                                ),
+                        ),
+                        const SizedBox(height: 10),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 24),
+                          child: Row(
+                            children: [
+                              const Text('SHARED',
+                                  style: TextStyle(
+                                    color: Color(0xFF959A9C),
+                                    fontSize: 16,
+                                    fontFamily: 'Rubik',
+                                    fontWeight: FontWeight.w700,
+                                    height: 0,
+                                  )),
+                              const SizedBox(width: 10),
+                              IconButton(
+                                icon: const Icon(Icons.add),
+                                onPressed: () {},
                               )
-                            : ListView.builder(
-                                shrinkWrap: true,
-                                itemCount: sharedWorkspaces.length,
-                                itemBuilder: (context, index) {
-                                  return DrawOption(
-                                    imgPath: "assets/later_icon.png",
-                                    text: sharedWorkspaces
-                                        .elementAt(index)
-                                        .title
-                                        .toString(),
-                                    func: () {},
-                                  );
-                                },
-                              ),
-                      ),
-                    ]),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          constraints: const BoxConstraints(maxHeight: 150),
+                          child: sharedWorkspaces.isEmpty
+                              ? const Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                )
+                              : ListView.builder(
+                                  shrinkWrap: true,
+                                  itemCount: sharedWorkspaces.length,
+                                  itemBuilder: (context, index) {
+                                    return DrawOption(
+                                      imgPath: "assets/later_icon.png",
+                                      text: sharedWorkspaces
+                                          .elementAt(index)
+                                          .title
+                                          .toString(),
+                                      func: () {},
+                                    );
+                                  },
+                                ),
+                        ),
+                      ]);
+                    }),
                   ]),
             ),
           ),
           Expanded(
             flex: 3,
-            child: ProfilePage(
-              username: widget.username,
-              emailAddress: widget.emailAddress,
-            ),
+            child: LayoutBuilder(builder: (context, constraints) {
+              if (currentUser?.id == 'rISCknyu5dlIrfGrKyCp') {
+                return const FreshPage();
+              }
+
+              return ProfilePage(
+                username: username,
+                emailAddress: emailAddress,
+              );
+            }),
           ),
         ],
       ),
