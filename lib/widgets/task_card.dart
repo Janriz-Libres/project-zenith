edaa2a2eec90b2477977d3c5ddcf60097f9e52be
dioexsys <dioexsys@gmail.com>
2023-12-03 13:58:05 +0800

    modified layout

--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -1,25 +1,50 @@
 import 'package:flutter/material.dart';
 
 class TaskCard extends StatelessWidget {
+  final String title;
+  final String desc;
 
   const TaskCard({
-    super.key,
+    super.key, required this.title, required this.desc,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
-      child: SizedBox(
-        width: 325,
-        height: 90,
-        child: Card(
-          color: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-              //set border radius more than 50% of height and width to make circle
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+            //set border radius more than 50% of height and width to make circle
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            width: 250,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                desc.isEmpty ? Container() : Text(
+                  desc,
+                  style: const TextStyle(
+                    fontFamily: "Rubik",
+                    fontSize: 12,
+                    color: Colors.black87
+                  ),
+                ),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontFamily: "Rubik",
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black87
+                  ),
+                ),
+              ],
+            ),
           ),
-          
         ),
       ),
     );
