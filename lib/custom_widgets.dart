fec409ca7c766e6678baeb59f04a683f953fbcbc
dioexsys <dioexsys@gmail.com>
2023-12-06 10:16:32 +0800

    added create task dialog

--- lib/custom_widgets.dart
+++ lib/custom_widgets.dart
@@ -991,3 +991,149 @@ class CreateWorkspaceDialog extends StatelessWidget {
     );
   }
 }
+
+class CreateTaskDialog extends StatelessWidget {
+  const CreateTaskDialog({
+    super.key,
+    required this.taskNameController,
+    required this.taskDescriptionController,
+    required this.func,
+  });
+
+  final TextEditingController taskNameController;
+  final TextEditingController taskDescriptionController;
+  final Future<void> Function() func;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: AlertDialog(
+        content: SizedBox(
+          width: 350,
+          child: Wrap(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 25, bottom: 35, left: 25, right: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(
+                      width: 250,
+                      child: Text(
+                        "Create new task",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: "Rubik",
+                            fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: const Icon(Icons.close))
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 5, left: 12, right: 12),
+                child: TextFormField(
+                  style: const TextStyle(fontSize: 14, color: Colors.white),
+                  controller: taskNameController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.black,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF06BCC1),
+                        width: 2,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF06BCC1),
+                        width: 2,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 2,
+                        color: Color(0xFF06BCC1),
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    contentPadding: const EdgeInsets.only(left: 20, right: 20),
+                    hintText: "Enter task name",
+                    hintStyle: const TextStyle(
+                        color: Color.fromARGB(255, 144, 142, 142),
+                        fontWeight: FontWeight.normal),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15, left: 12, right: 12),
+                child: TextFormField(
+                  maxLines: 3,
+                  style: const TextStyle(fontSize: 14, color: Colors.white),
+                  controller: taskDescriptionController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.black,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF06BCC1),
+                        width: 2,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Color(0xFF06BCC1),
+                        width: 2,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        width: 2,
+                        color: Color(0xFF06BCC1),
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    contentPadding:
+                        const EdgeInsets.only(top: 30, left: 20, right: 20),
+                    hintText: "Enter task description",
+                    hintStyle: const TextStyle(
+                        color: Color.fromARGB(255, 144, 142, 142),
+                        fontWeight: FontWeight.normal),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 25, right: 15),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: SubmitButton(
+                    text: "Enter",
+                    gradient: const [Color(0xFF06BCC1), Color(0xFF168285)],
+                    minSize: const Size(200, 50),
+                    func: () async {
+                      await func();
+                      if (context.mounted) {
+                        Navigator.pop(context);
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
